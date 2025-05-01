fd5a0c5477832fbee6748b33588bdb31ecd20bb6
Aleksei Savin <saleksey67@gmail.com>
2025-05-01 21:15:13 +0300

    backtest ok if more 1 order pushed

--- jesse/strategies/RLStrategy/__init__.py
+++ jesse/strategies/RLStrategy/__init__.py
@@ -5,44 +5,64 @@ from jesse.rl.Agent import Agent  # import RL agent
 import jesse.indicators as ta
 
 class RLStrategy(Strategy):
+
     def __init__(self):
         super().__init__()
         self.agent = Agent(...)  # Initialize RL-agent
 
-    def should_long(self) -> bool:
-        # action = self.agent.select_action(self.get_state())  # Get RL-agent state
-        # if action == 1:  # Buy action
-        #     return True
+    @property
+    def is_short(self) -> bool:
+        action = self.agent.select_action(self.get_state())  # Get RL-agent state
+        if action == 2:  # Sell action
+            return True
         return False
+
+    @property
+    def is_long(self) -> bool:
+        action = self.agent.select_action(self.get_state())  # Get RL-agent state
+        if action == 1:  # Buy action
+            return True
+        return False
+
+    def should_long(self) -> bool:
+        if self.is_long and not self.is_short:
+            return True
+        return None
 
     def should_short(self) -> bool:
-        # TODO: fix
-        # action = self.agent.select_action(self.get_state())  # Get RL-agent state
-        # if action == 2:  # Sell action
-        #     return True
-        return False
-
-    def go_long(self):
-        qty = self.available_margin * 0.99
-        self.buy = qty, self.price
-
-    def go_short(self):
-        qty = self.available_margin * 0.99
-        self.sell = qty, self.price
+        if self.is_short and not self.is_long:
+            return True
+        return None
 
     def should_cancel(self):
         return False
+
+    def go_long(self):
+        # qty = self.available_margin * 0.99
+        qty = 0.01
+        self.buy = qty, self.price
+        pass
+
+    def go_short(self):
+        # qty = self.available_margin * 0.99
+        qty = 0.01
+        self.sell = qty, self.price
+        pass
 
     def get_state(self):
         # Market state
         # Example: using last close, volume and RSI
         close = self.candles[:, 2]
-        volume = self.candles[:, 5]
+        # volume = self.candles[:, 5]
         # rsi = ta.rsi(close, period=14)
 
         # create PyTorch tensor
         # state = torch.tensor([close[-1], volume[-1], rsi[-1]])
-        state = {}
+        # state = {}
+        state = torch.tensor([close[-1]])
 
 
         return state
+
+    def lazy_calc_proba(self):
+        pass
--- tests/test_rl_strategy.py
+++ tests/test_rl_strategy.py
@@ -9,24 +9,10 @@ from jesse.modes import backtest_mode
 from jesse.routes import router
 from jesse.store import store
 from jesse.config import config
-from jesse.config import reset_config
+from jesse.config import reset_config, set_config
 from tests.data import test_candles_1
+from jesse.testing_utils import set_up
 
-
-
-def set_up(is_futures_trading=True, leverage=1, leverage_mode='cross', fee=0):
-    reset_config()
-    config['env']['exchanges'][exchanges.SANDBOX]['balance'] = 10_000
-
-    config['env']['exchanges']['Sandbox']['fee'] = fee
-
-    if is_futures_trading:
-        # used only in futures trading
-        config['env']['exchanges'][exchanges.SANDBOX]['type'] = 'futures'
-        config['env']['exchanges'][exchanges.SANDBOX]['futures_leverage_mode'] = leverage_mode
-        config['env']['exchanges'][exchanges.SANDBOX]['futures_leverage'] = leverage
-    else:
-        config['env']['exchanges'][exchanges.SANDBOX]['type'] = 'spot'
 
 class TestRLStrategy(unittest.TestCase):
     def test_rl_strategy(self):
@@ -45,26 +31,23 @@ class TestRLStrategy(unittest.TestCase):
         }
         backtest_mode.run('000', False, {}, exchanges.SANDBOX, routes, [], '2019-04-01', '2019-04-02', candles)
 
-        assert len(store.completed_trades.trades) == 0
+        assert len(store.completed_trades.trades) > 0
 
         # Checking results
         # assert ...
 
-
         # Checking that strategy was created and launched
         # self.assertIsNotNone(router.routes[0].strategy)
         # self.assertIsInstance(router.routes[0].strategy, RLStrategy)
-        
+
         # Checking that agent was initialized
         # self.assertIsNotNone(router.routes[0].strategy.agent)
         # self.assertIsInstance(router.routes[0].strategy.agent, Agent)
-        
+
         # Additional checks may include:
         # - checking agent state
         # - checking that agent can get state from environment
         # - checking that agent can select actions
-
-
 
 
 rl = RLStrategy()
