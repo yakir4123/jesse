ae61f705b0062c1eb2bde79b8e6903e44c2a99b6
Aleksei Savin <saleksey67@gmail.com>
2025-05-03 22:32:43 +0300

    try to build best solution

--- jesse/strategies/RLStrategy/__init__.py
+++ jesse/strategies/RLStrategy/__init__.py
@@ -1,14 +1,50 @@
 # jesse/strategies/RLStrategy.py
+from typing import List, Dict
+
 import torch
-from jesse.strategies import Strategy
-from jesse.rl.Agent import Agent  # import RL agent
+
 import jesse.indicators as ta
+from jesse.models import Route, Position, Order
+from jesse.rl.Agent import Agent  # import RL agent
+from jesse.store import ClosedTrades
+from jesse.strategies import Strategy
+
 
 class RLStrategy(Strategy):
 
     def __init__(self):
         super().__init__()
-        self.agent = Agent(...)  # Initialize RL-agent
+        self.agent = Agent(
+            MODEL='ddqn',
+            DOUBLE=True,
+
+        )  # Initialize RL-agent
+
+    # region Agent
+
+    # def select_action(self, state):
+    #     pass
+
+    # def optimize_model(self):
+    #     pass
+
+    # def optimize_double_dqn_model(self):
+    #     pass
+
+    # def train(self, env, path, num_episodes=40):
+    #     pass
+
+    # def test(self, env_test, model_name=None, path=None):
+    #     pass
+
+    # endregion
+
+    # region Strategy
+    # def hyperparameters(self) -> list:
+    #     return []
+    #
+    # def dna(self) -> str:
+    #     return ''
 
     @property
     def is_short(self) -> bool:
@@ -64,5 +100,72 @@ class RLStrategy(Strategy):
         # state = torch.tensor([close[-1], volume[-1], rsi[-1]])
         return state
 
-    def lazy_calc_proba(self):
-        pass
+
+    #
+    # def lazy_calc_proba(self):
+    #     pass
+    #
+    # def has_long_entry_orders(self) -> bool:
+    #     pass
+    #
+    # def has_short_entry_orders(self) -> bool:
+    #     pass
+    #
+    # def liquidate(self):
+    #     pass
+    #
+    # def routes(self) -> List[Route]:
+    #     pass
+    #
+    # def leverage(self) -> int:
+    #     pass
+    #
+    # def all_positions(self) -> Dict[str, Position]:
+    #     pass
+    #
+    # def portfolio_value(self) -> float:
+    #     pass
+    #
+    # def trades(self) -> List[ClosedTrades]:
+    #     pass
+    #
+    # def orders(self) -> List[Order]:
+    #     pass
+    #
+    # def entry_orders(self):
+    #     pass
+    #
+    # def exit_orders(self):
+    #     pass
+    #
+    # def active_exit_orders(self):
+    #     pass
+    #
+    # def exchange_type(self):
+    #     pass
+    #
+    # def is_spot_trading(self) -> bool:
+    #     pass
+    #
+    # def is_futures_trading(self) -> bool:
+    #     pass
+    #
+    # def daily_balances(self):
+    #     pass
+    #
+    # def is_backtesting(self) -> bool:
+    #     pass
+    #
+    # def is_livetrading(self) -> bool:
+    #     pass
+    #
+    # def is_papertrading(self) -> bool:
+    #     pass
+    #
+    # def is_live(self) -> bool:
+    #     pass
+    #
+    # def min_qty(self) -> float:
+    #     pass
+
+    # endregion
